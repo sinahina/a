5f9014faca4e1d2490ebf33eba3f4a8cc0714fc6
sinahina <sinamonarch@gmail.com>
2016-09-22 17:38:04 +0330

    Add files via upload

---
+++ 23/plugins/Vc.lua
@@ -0,0 +1,32 @@
+--[[
+
+اهنگ احمد سعیدی(تحمل کن
+	 
+]]
+local function run(msg, matches)
+  local eq = matches[1]
+
+  local url = "http://www.farsireader.com/PlayText.aspx?Text="..URL.escape(eq).."&Punc=false"
+  local receiver = get_receiver(msg)
+  local file = download_to_file(url,'text.ogg')
+  send_audio('channel#id'..msg.to.id, file, ok_cb , false)
+  send_audio('chat#id'..msg.to.id, file, ok_cb , false)
+
+end
+
+return {
+  description = "Convert text to voice",
+  usage = {
+    "tts [text]: Convert text to voice"
+  },
+  patterns = {
+    "^[#!/]vc (.+)$",
+    "^[Vc] (.+)$",
+  },
+  run = run
+}
+--[[
+
+     اهنگ احمد سعیدی(تحمل کن
+	 
+]]
